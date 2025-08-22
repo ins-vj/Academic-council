ddcd2c1363ae824e977c0a1d834cfb4efbeb8840
Chetan Verma <coutcv28@gmail.com>
2025-08-22 17:01:51 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,103 +1,95 @@
-import Image from "next/image";
+"use client"; 
 
-export default function Home() {
+import React, { useRef, useState } from "react";
+
+interface UserData {
+  email: string;
+  password: string;
+}
+
+const Login: React.FC = () => {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const [userData, setUserData] = useState<UserData>({ email: "", password: "" });
+
+  const handleEnterKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    ref: React.RefObject<HTMLInputElement> | null
+  ) => {
+    if (e.key === "Enter" && ref?.current) {
+      e.preventDefault();
+      ref.current.focus();
+      console.log((e.target as HTMLInputElement).value);
+    }
+  };
+
+  const handleSubmit = () => {
+    if (userData.email === "") {
+      alert("Please enter email");
+    } else if (userData.password === "") {
+      alert("Please enter password");
+    } else {
+      console.log(userData);
+      console.log(userData.email);
+      console.log(userData.password);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="bg-[url('https://academic.iiti.ac.in/app/public/assets/img/login-bg/2.jpg')] bg-cover bg-center w-screen h-screen fixed">
+      <div className="bg-blue-700/30 backdrop-blur-xs w-full h-full flex justify-center items-center px-5">
+        <div className="loginCard bg-[#272757]/35 flex flex-col items-center rounded-2xl w-fit px-5 sm:px-10 py-10 shadow-[10px_10px_10px_rgba(0,0,0,0.5)]">
+          <div className="title flex items-center gap-5 mb-5">
+            <img
+              className="rounded-2xl"
+              src="/acadlogo.jpg"
+              width={100}
+              alt="Academic Council"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <h1 className="text-4xl font-bold text-center">
+              <span>Academic Council </span> | <span> IIT Indore</span>
+            </h1>
+          </div>
+
+          <input
+            value={userData.email}
+            name="email"
+            onChange={handleChange}
+            ref={emailRef}
+            onKeyDown={(e) => handleEnterKeyDown(e, passwordRef)}
+            type="text"
+            placeholder="Email id"
+            className="bg-white/75 text-black/80 text-lg mb-5 px-5 py-2 rounded-lg focus:shadow-[10px_10px_10px_rgba(0,0,0,0.5)] outline-none w-[280px] sm:w-[450px] lg:w-[500px] transition-all duration-100 ease-in-out"
+          />
+
+          <input
+            value={userData.password}
+            name="password"
+            onChange={handleChange}
+            ref={passwordRef}
+            onKeyDown={(e) => handleEnterKeyDown(e, null)}
+            type="password"
+            placeholder="Password"
+            className="bg-white/75 text-black/80 text-lg mb-5 px-5 py-2 rounded-lg focus:shadow-[10px_10px_10px_rgba(0,0,0,0.5)] outline-none w-[280px] sm:w-[450px] lg:w-[500px] transition-all duration-100 ease-in-out"
+          />
+
+          <div className="flex justify-center mb-5">
+            <button
+              onClick={handleSubmit}
+              className="bg-white/75 hover:bg-white/50 text-black/70 hover:text-black/80 px-5 py-2 text-lg font-bold rounded-xl cursor-pointer hover:scale-110 transition-all duration-300 ease-in-out"
+            >
+              Sign in
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
-}
+};
+
+export default Login;
